721506b6bc6056404ba720a211be6c3a3c09f79d
Gregory Redig <gregoryredig@Gregorys-MacBook-Pro.local>
2019-08-23 14:23:13 -0700

    connect mongoDB atlas

---
+++ config/database.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true
+});
+
+mongoose.connection.on('connected', function() {
+    console.log('Connected to MongoDB at ${process.env.DATABASE_URL}');
+});
+
+module.exports = mongoose;
--- server.js
+++ server.js
@@ -4,6 +4,12 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// Configures .env
+require('dotenv').config();
+
+// Require database config file {connect to DB}
+require('./config/database');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
